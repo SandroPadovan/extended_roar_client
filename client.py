792e73d6af66e27a4eeac26649ae657ce83b4dc5
SandroPadovan <sandropadovan@bluewin.ch>
2023-10-27 11:10:28 +0200

    feat: added command line argument to execute benign behavior scripts

--- client.py
+++ client.py
@@ -4,11 +4,35 @@ from multiprocessing import Process
 from socket import AF_INET, SOCK_STREAM, socket
 from subprocess import call
 from time import sleep
+from argparse import ArgumentParser
 
 from globals import get_config_path, update_existing_config
 from rwpoc import run
 
 TARGET_PATH = "<start-path-on-target-device>"
+
+
+def parse_args():
+    parser = ArgumentParser(description='Electrosense Sensor')
+    parser.add_argument("-b", "--behaviors",
+                        nargs="*",
+                        choices=["normal", "compression", "installation", "preprocessing"],
+                        help="Additional benign behavior executed.",
+                        default="normal",
+                        action="store")
+    return parser.parse_args()
+
+
+def execute_compression_behavior():
+    call("./benign_behaviors/compression.sh")
+
+
+def execute_installation_behavior():
+    call("./benign_behaviors/installation.sh")
+
+
+def execute_preprocessing_behavior():
+    call("./benign_behaviors/preprocessing.sh")
 
 
 def listen_for_config_changes():
@@ -52,6 +76,21 @@ if __name__ == "__main__":
     proc_fp = Process(target=collect_device_fingerprint)
     procs.append(proc_fp)
     proc_fp.start()
+
+    args = parse_args()
+    behaviors = args.behaviors
+
+    def start_behavior_process(target):
+        behavior_process = Process(target=target)
+        procs.append(behavior_process)
+        behavior_process.start()
+
+    if "compression" in behaviors:
+        start_behavior_process(execute_compression_behavior)
+    if "installation" in behaviors:
+        start_behavior_process(execute_installation_behavior)
+    if "preprocessing" in behaviors:
+        start_behavior_process(execute_preprocessing_behavior)
 
     print("Waiting for initial config...")
     while not path.exists(config_path):
